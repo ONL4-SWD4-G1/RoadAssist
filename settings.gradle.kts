4a38d5b3320b7c22fcccfd1080de58aa3649ae8b
Karim Al-Zmzmy <karam.zmzmy@gmail.com>
2026-04-19 20:14:54 +0200

    chore: initial clean setup with proper gitignore

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RoadAssist"
+include(":app-user")
+include(":app-mechanic")
+include(":app-admin")
