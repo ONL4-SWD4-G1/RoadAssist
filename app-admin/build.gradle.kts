e39c62968bd6998015025f520ee8a4d95a76f6d3
Karim Al-Zmzmy <karam.zmzmy@gmail.com>
2026-04-26 21:54:26 +0300

    build: define and use dependency bundles in version catalog

--- app-admin/build.gradle.kts
+++ app-admin/build.gradle.kts
@@ -40,17 +40,14 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.bundles.compose.ui)
+
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.bundles.android.test)
+    androidTestImplementation(libs.bundles.compose.test)
+
+    debugImplementation(libs.bundles.compose.debug)
 }
